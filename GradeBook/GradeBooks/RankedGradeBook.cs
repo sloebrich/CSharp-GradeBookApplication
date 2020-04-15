6472e7ff0c5d53dd676d5000bfc93e3fb75922ac
Steffen Löbrich <steffen.loebrich@gmx.de>
2020-04-15 19:09:29 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,30 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            double numberOfHigherGrades = Students.FindAll(x => x.AverageGrade > averageGrade).Count;
+            double percentile = Math.Floor(5* numberOfHigherGrades / Students.Count);
+
+            switch (percentile)
+            {
+                case 0:
+                    return 'A';
+                case 1:
+                    return 'B';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';
+            }
+
+            return 'F';
+
+        }
     }
 }
